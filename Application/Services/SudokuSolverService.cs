c902d582cd5eb7454a62bc8918b76561421b40c0
Alex <ajwaarts@avans.nl>
2022-06-01 08:56:34 +0200

    initial setup/

---
+++ Application/Services/SudokuSolverService.cs
@@ -0,0 +1,19 @@
+using DPAT_eindopdracht.Application.Algorithms;
+using DPAT_eindopdracht.Domain.Board;
+
+namespace DPAT_eindopdracht.Application.Services;
+
+public class SudokuSolverService
+{
+    private ISudokuAlgorithm algorithm;
+
+    public SudokuSolverService(ISudokuAlgorithm algorithm)
+    {
+        this.algorithm = algorithm;
+    }
+
+    public IBoard SolveSudoku(IBoard board)
+    {
+        return algorithm.SolveSudoku(board);
+    }
+}
---
+++ Domain/Cell/CellFactory.cs
@@ -0,0 +1,11 @@
+namespace DPAT_eindopdracht.Domain.Cell;
+
+public class CellFactory
+{
+    protected List<Cell> cellTypes;
+
+    public Cell createCell(string state)
+    {
+        throw new NotImplementedException();
+    }
+}
